9c4c63a7be0bae4fb5246ac6a87c4e32f01835de
jerrysdesign <jerry7722@hotmail.com>
2015-08-02 10:47:46 +0800

    use full page,js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,4 +2,31 @@
 
 var myApp = myApp || {};
 
-console.log('Hodd luck!');
+$(document).ready(function() {
+
+
+
+  /*---------------- full page scroll -----------------*/
+  $('#fullpage').fullpage({
+    //Scrolling
+    css3: true,
+    scrollingSpeed: 700,
+    autoScrolling: true,
+    fitToSection: true,
+    scrollBar: false,
+    easing: 'easeInOutCubic',
+    easingcss3: 'ease',
+    loopBottom: false,
+    loopTop: false,
+    loopHorizontal: true,
+    continuousVertical: false,
+    normalScrollElements: '#element1, .element2',
+    scrollOverflow: false,
+    touchSensitivity: 15,
+    normalScrollElementTouchThreshold: 5,
+  });
+
+
+
+});
+
