af41b131bcfdd93b6a55caac91a4467ca3fa7965
jerrysdesign <jerry7722@hotmail.com>
2015-08-03 21:49:56 +0800

    新增漢堡選單

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,6 +5,12 @@ var myApp = myApp || {};
 $(document).ready(function() {
 
 
+   /*---------------- side menu -----------------*/
+  $('.button-nav').on('click', function(){
+    $('.nav-menu').addClass('nav-menu-open');
+    $('body').css('overflow-y','hidden');
+  });
+
 
   /*---------------- full page scroll -----------------*/
   $('#fullpage').fullpage({
